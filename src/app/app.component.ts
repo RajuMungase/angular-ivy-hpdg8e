632327fc1cc4f8541aa75b50be9dc18f7113eb55
RajuMungase <rajumungase.rm@gmail.com>
2022-03-26 16:22:38 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component, VERSION } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  VERSION,
+} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent
+  implements OnInit, OnChanges, AfterViewInit, OnDestroy
+{
   name = 'Angular ' + VERSION.major;
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    console.log('OnInit');
+  }
+  ngAfterViewInit() {
+    console.log('AfterViewInit');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
 }
